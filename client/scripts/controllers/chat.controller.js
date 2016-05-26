c037594d5c209c9328a45a7a6b27abb8545b71c7
quarbby <lynnette-ng@outlook.com>
2016-05-26 22:06:07 +0800

    3.24

--- client/scripts/controllers/chat.controller.js
+++ client/scripts/controllers/chat.controller.js
@@ -1,4 +1,5 @@
 import Ionic from 'ionic-scripts';
+import { _ } from 'meteor/underscore';
 import {Meteor} from 'meteor/meteor';
 import {Controller} from 'angular-ecmascript/module-helpers';
 import {Chats, Messages} from '../../../lib/collections';
@@ -22,7 +23,15 @@ export default class ChatCtrl extends Controller {
     }
     
     sendMessage() {
+        if (_.isEmpty(this.message)) return;
         
+        Meteor.call('newMessage', {
+            text: this.message,
+            type: 'text',
+            chatId: this.chatId
+        });
+        
+        delete this.message;
     }
     
     inputUp() {
