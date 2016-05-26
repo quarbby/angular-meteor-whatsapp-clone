822116637e852314badb40562fda24cc757292b5
quarbby <lynnette-ng@outlook.com>
2016-05-26 09:44:33 +0800

    1.12

--- client/scripts/lib/app.js
+++ client/scripts/lib/app.js
@@ -4,31 +4,34 @@ import 'angular-meteor';
 import 'angular-sanitize';
 import 'angular-ui-router';
 import 'ionic-scripts';
-import Angular from 'angular';
 import Loader from 'angular-ecmascript/module-loader';
+import Angular from 'angular';
 import { Meteor } from 'meteor/meteor';
-
+ 
 // Modules
+import ChatsCtrl from '../controllers/chats.controller';
 import RoutesConfig from '../routes';
 
 const App = 'Whatsapp';
-
+ 
 // App
 Angular.module(App, [
-   'angular-meteor',
-   'ionic' 
+  'angular-meteor',
+  'ionic'
 ]);
 
 new Loader(App)
+    .load(ChatsCtrl)
     .load(RoutesConfig);
 
 // Startup
-if (Meteor.isCordava) {
-    Angular.element(document).on('deviceready', onReady);
-} else {
-    Angular.element(document).ready(onReady);
+if (Meteor.isCordova) {
+  Angular.element(document).on('deviceready', onReady);
 }
-
+else {
+  Angular.element(document).ready(onReady);
+}
+ 
 function onReady() {
-    Angular.bootstrap(document, [App]);
+  Angular.bootstrap(document, [App]);
 }
--- client/scripts/routes.js
+++ client/scripts/routes.js
@@ -1,23 +1,25 @@
-import {Config} from 'angular-ecmascript/module-helpers';
-
+import { Config } from 'angular-ecmascript/module-helpers';
+ 
 export default class RoutesConfig extends Config {
-    configure() {
-        this.$stateProvider
-            .state('tab', {
-                url: '/tab',
-                abstract: true,
-                templateUrl: 'client/templates/tabs.html'
-            })
-            .state('tab.chats', {
-                url: '/chats',
-                views: {
-                    'tab-chats': {
-                        templateUrl: 'client/templates/chats.html'
-                    }
-                }
-            });
-         this.$urlRouterProvider.otherwise('tab/chats');
-    }
+  configure() {
+    this.$stateProvider
+      .state('tab', {
+        url: '/tab',
+        abstract: true,
+        templateUrl: 'client/templates/tabs.html'
+      })
+      .state('tab.chats', {
+        url: '/chats',
+        views: {
+          'tab-chats': {
+            templateUrl: 'client/templates/chats.html',
+            controller: 'ChatsCtrl'
+           }
+        }
+      });
+ 
+    this.$urlRouterProvider.otherwise('tab/chats');
+  }
 }
-
+ 
 RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
