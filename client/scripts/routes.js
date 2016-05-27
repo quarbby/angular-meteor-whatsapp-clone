e58ce1c32552a899dabb97ce3331d131b3d7c0fc
quarbby <lynnette-ng@outlook.com>
2016-05-27 21:11:46 +0800

    4.12

--- client/scripts/routes.js
+++ client/scripts/routes.js
@@ -1,12 +1,21 @@
-import { Config } from 'angular-ecmascript/module-helpers';
- 
-export default class RoutesConfig extends Config {
+import {_} from 'meteor/underscore';
+import { Config, Runner } from 'angular-ecmascript/module-helpers';
+  
+class RoutesConfig extends Config {
+  constructor() {
+    super(...arguments);
+    this.isAuthorized = ['$auth', this.isAuthorized.bind(this)];
+  }
+  
   configure() {
     this.$stateProvider
       .state('tab', {
         url: '/tab',
         abstract: true,
-        templateUrl: 'client/templates/tabs.html'
+        templateUrl: 'client/templates/tabs.html',
+        resolve: {
+          user: this.isAuthorized
+        }
       })
       .state('tab.chats', {
         url: '/chats',
@@ -39,12 +48,35 @@ export default class RoutesConfig extends Config {
       .state('profile', {
         url: '/profile',
         templateUrl: 'client/templates/profile.html',
-        controller: 'ProfileCtrl as profile'
+        controller: 'ProfileCtrl as profile',
+        resolve: {
+          user: this.isAuthorized
+        }
       })
       ;
  
     this.$urlRouterProvider.otherwise('tab/chats');
   }
+  
+  isAuthorized($auth) {
+    return $auth.awaitUser();
+  }
 }
  
 RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+class RoutesRunner extends Runner {
+  run() {
+    this.$rootScope.$on('$stateChangeError', (...args) => {
+      const err = _.last(args);
+      
+      if (err === 'AUTH_REQUIRED') {
+        this.$state.go('login');
+      }
+    });
+  }
+}
+
+RoutesRunner.$inject = ['$rootScope', '$state'];
+
+export default [RoutesConfig, RoutesRunner];
